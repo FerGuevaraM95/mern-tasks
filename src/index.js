9db10cc0f13c9efc4c272a540644924b81878225
Fernando Guevara <luisfernandoguevara95@gmail.com>
2018-11-12 18:46:36 -0600

    (Add): general structure [:root]

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+//Routes
+app.use('/api/tasks', require('./routes/tasks.routes'));
+
+//Static
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start Server
+app.listen(app.get('port'), ()=> {
+    console.log(`Server on Port ${app.get('port')}`);
+});
